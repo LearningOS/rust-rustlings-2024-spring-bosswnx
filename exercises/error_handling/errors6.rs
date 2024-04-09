dd0d3ff125b4d679942b246c00b23085fec061d5
Nelson Boss <bosswnx@qq.com>
2024-04-09 18:28:28 +0800

    完成 59/110 (54.5%)

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,8 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -24,6 +22,9 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
     // TODO: add another error conversion function here.
     // fn from_parseint...
 }
@@ -31,7 +32,7 @@ impl ParsePosNonzeroError {
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;  // 有点抽象...
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -16,6 +16,13 @@ trait AppendBar {
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
 
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {  // 这里竟然可以用 mut？
+        self.push("Bar".to_string());
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
