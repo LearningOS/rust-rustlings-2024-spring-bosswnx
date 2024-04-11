6e9763cbb7e4676e7545eece092d45e87dfa52c5
Nelson Boss <bosswnx@qq.com>
2024-04-11 20:19:40 +0800

    完成 98/110 (88.2%)

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,8 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 /// # Safety
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
@@ -31,8 +29,9 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+    let p = address as *mut u32;
     unsafe {
-        &address = 0xAABBCCDD; 
+        *p = 0xAABBCCDD; 
     }
 }
 
